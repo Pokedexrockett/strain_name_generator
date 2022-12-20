e71390a4fce8637a21630fa89ccbdfe1c55168af
Dexter Crockett <dex429.dc@gmail.com>
2022-12-20 02:25:28 +0000

    first commit

---
+++ strain_name_generator/main.py
@@ -0,0 +1,10 @@
+#1. Create a greeting for your program.
+print("Welcome to the strain name generator!")
+#2. Ask the user for the city that they grew up in.
+color = input("What color shirt are you wearing?\n")
+#3. Ask the user for the name of a pet.
+animal = input("What is favorite animal?\n")
+#4. Combine the name of their city and pet and show them their band name.
+number = input("Pick a number between 1 and 10?\n")
+print("Your strain name is " + color + " " + animal + "#" + number + "!")
+#5. Make sure the input cursor shows on a new line:
